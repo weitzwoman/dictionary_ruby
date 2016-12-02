40b02c51632c21c3070632c697486fb13c5b2f5a
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-02 08:16:10 -0800

    passes second spec, finds empty array for definition

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,8 +5,12 @@ require('word')
 describe(Word) do
   describe("#initialize") do
     it('tests for the word method and return word') do
-      test_word = Word.new({:word => "sequioa", :definition => "redwood tree"})
+      test_word = Word.new({:word => "sequioa", :definition => []})
       expect(test_word.word).to(eq("sequioa"))
+    end
+    it('tests for the definition method; initially empty') do
+      test_word = Word.new({:word => "sequioa", :definition => []})
+      expect(test_word.definition).to(eq([]))
     end
   end
 end
